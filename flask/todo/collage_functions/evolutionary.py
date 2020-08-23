da76c9caf460b0383c442170621a8353b10c4aa0
Dorothy <35271019+dorothyqu@users.noreply.github.com>
2020-08-23 13:57:56 -0400

    fixed moving issues

---
+++ flask/todo/collage_functions/evolutionary.py
@@ -0,0 +1,115 @@
+import glob
+import json
+import os
+import sys
+
+from flask.todo.collage_functions import colorpalette
+from flask.todo.collage_functions.Collage import Collage
+from flask.todo.collage_functions.colorpalette import *
+import random
+
+path= "../decades/cat/"
+
+PATH_PRE = "../static/"
+FILE_NAME = None
+
+# input: probability that it will be a 1
+def initializeValue(p):
+    # 0 = don't change, 1 = change
+    if random.choices([0, 1], [1 - p, p]) == [1]:
+        return 1
+    else:
+        return 0
+
+def initializeRotation():
+    # change the number to something along the distribution
+    if random.choices([0, 1], [.4, .6]) == [0]:
+        return np.random.normal(0, 5)
+    # return the same value
+    else:
+        return 0
+
+# initialize genes for a parent collage
+def initializeGenes(fName):
+    # GENE TIME
+    # set up the paths
+    imagenames = glob.glob(path+"*")
+    texturenames = glob.glob("../textures/*")
+    brushnames = glob.glob("../brushes/*")
+
+    p = np.random.normal(.5, .1)
+    color = colorpalette.randomcolor()
+    palette = random.randint(1, 4)
+
+    imageprob = random.random() # do we want tons of pics or not many
+    images = [initializeValue(.3) for i in imagenames]
+
+    edits = []
+    masks = [initializeValue(.05) for i in imagenames]
+    blur = [initializeValue(.05) for i in imagenames]
+    rotate = [initializeRotation() for i in imagenames]
+    tint = [initializeValue(.05) for i in imagenames]
+    textures = [0, 1]
+    brushes = [initializeValue(.3) for i in brushnames]
+
+    edits.append(masks)
+    edits.append(blur)
+    edits.append(rotate)
+    edits.append(tint)
+
+    # saveies genes
+    with open(fName, 'w+') as outfile:
+        json.dump({
+            "imagenames": imagenames,
+            "p": p,
+            "color": color,
+            "palette": palette,
+            "images": images,
+            "edits": edits,
+            "textures": textures,
+            "texturenames": texturenames,
+            "brushes": brushes,
+            "brushnames": brushnames
+        }, outfile, indent=2)
+
+def setup():
+    global colors, pos, collage
+    size(900,1100)
+
+    # save the json
+    jsonPath = "{}{}.json".format(PATH_PRE, FILE_NAME)
+    initializeGenes(jsonPath)
+
+    # load up genes
+    with open(jsonPath, 'r') as infile:
+        genes = json.load(infile)
+
+    # actually set up the collage
+    collage = Collage(genes)
+    collage.setup()
+
+def draw():
+    collage.draw()
+
+    fullPath = "{}{}".format(PATH_PRE, FILE_NAME)
+    correctName = "{}.png".format(fullPath)
+    # saving adds 4 0s for some reason
+    save(correctName)
+
+    # so fix it lmao
+    os.rename("{}0000.png".format(fullPath), correctName)
+
+    # all done
+    exit()
+
+if __name__ == '__main__':
+
+    # get the full paths for the json and png files
+    defFileName = "collage"
+    if len(sys.argv) > 1: # first arg is the name for the file ex. "collage11"
+        FILE_NAME = sys.argv[1]
+    else:
+        print("No filename supplied, defaulting to '{}'".format(defFileName))
+        FILE_NAME = defFileName
+
+    run()
